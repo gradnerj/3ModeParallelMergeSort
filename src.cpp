742cca0ad0cb32aec30463a5405d36786ff033e6
Jeff <gradnerj@gmail.com>
2021-03-22 22:42:59 +0000

    Done with L1, obtained new L and new H via permute

--- src.cpp
+++ src.cpp
@@ -37,5 +37,23 @@ int main(){
 
 	print_vector_int(l, "low");
 	print_vector_int(h, "high");
+	
+	int l_idx[] = {0,1,2,3,4,5,6,7,16,17,18,19,20,21,22,23};
+	int h_idx[] = {8,9,10,11,12,13,14,15,24,25,26,27,28,29,30,31};
+
+	int *l_idx_ptr = (int*)aligned_alloc(64, sizeof(int)*16);
+	int *h_idx_ptr = (int*)aligned_alloc(64, sizeof(int)*16);
+	for(int i = 0; i < 16; i++){
+		l_idx_ptr[i] = l_idx[i];
+		h_idx_ptr[i] = h_idx[i];
+	}	
+	
+	__m512i l_idx_vec = _mm512_load_si512(&l_idx_ptr[0]);
+	__m512i h_idx_vec = _mm512_load_si512(&h_idx_ptr[0]);
+
+	__m512i new_l = _mm512_permutex2var_epi32(l,l_idx_vec,h);
+	__m512i new_h = _mm512_permutex2var_epi32(l,h_idx_vec,h);
+	print_vector_int(new_l, "New L");
+	print_vector_int(new_h, "New H");
     return 0;
 }
